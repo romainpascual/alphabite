1f041fa36a31dc77885efe677902d2a3d86d40b4
Arthur Caillaud <arthurcaillaud@gmail.com>
2019-02-21 14:29:18 +0100

    Adding getters for Move

--- board_generator/move.py
+++ board_generator/move.py
@@ -8,3 +8,12 @@ class Move:
         new_board = board.exec_move_src(self.__src_cell, self.__change)
         new_board = new_board.exec_move_dest(self.__dest_cell, self.__change)
         return new_board
+
+    def get_src_cell():
+        return self.__src_cell
+
+    def get_dest_cell():
+        return self.__dest_cell
+
+    def get_change():
+        return self.__change
