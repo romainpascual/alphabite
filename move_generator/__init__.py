ec5e28f25024da4ed98f029628ea2bec84fc801a
Arthur Caillaud <arthurcaillaud@gmail.com>
2019-02-21 14:13:36 +0100

    MoveBottomRight was wrong

--- move_generator/__init__.py
+++ move_generator/__init__.py
@@ -15,6 +15,7 @@ class MoveGenerator:
         board_height = self.__board.get_height()
         x0 = src_cell.get_x()
         y0 = src_cell.get_y()
+
         ###
         # IMPORTANT
         # For now we do not split our population
@@ -22,38 +23,47 @@ class MoveGenerator:
         # TODO? - Enable splitting moves
         ###
         change = Change(src_cell.get_value(), src_cell.get_species())
+
         if (x0 > 0): # Move left
             dest_cell = self.__board.get_cell((x0 - 1, y0))
             possible_move = Move(src_cell, dest_cell, change)
             possible_moves.append(possible_move)
-        if (x0 > 0 and y0 > 0): # Move bottom left
-            dest_cell = self.__board.get_cell((x0 - 1, y0 - 1))
-            possible_move = Move(src_cell, dest_cell, change)
-            possible_moves.append(possible_move)
-        if (x0 > 0 and y0 < board_height - 1): # Move up left
-            dest_cell = self.__board.get_cell((x0 - 1, y0 + 1))
-            possible_move = Move(src_cell, dest_cell, change)
-            possible_moves.append(possible_move)
-        if (y0 < board_height - 1): # Move up
-            dest_cell = self.__board.get_cell((x0, y0 + 1))
-            possible_move = Move(src_cell, dest_cell, change)
-            possible_moves.append(possible_move)
-        if (y0 > 0): # Move bottom
-            dest_cell = self.__board.get_cell((x0, y0 - 1))
-            possible_move = Move(src_cell, dest_cell, change)
-            possible_moves.append(possible_move)
+
         if (x0 < board_width - 1): # Move right
             dest_cell = self.__board.get_cell((x0 + 1, y0))
             possible_move = Move(src_cell, dest_cell, change)
             possible_moves.append(possible_move)
+
+        if (y0 > 0): # Move bottom
+            dest_cell = self.__board.get_cell((x0, y0 - 1))
+            possible_move = Move(src_cell, dest_cell, change)
+            possible_moves.append(possible_move)
+
+        if (y0 < board_height - 1): # Move up
+            dest_cell = self.__board.get_cell((x0, y0 + 1))
+            possible_move = Move(src_cell, dest_cell, change)
+            possible_moves.append(possible_move)
+
+        if (x0 > 0 and y0 > 0): # Move bottom left
+            dest_cell = self.__board.get_cell((x0 - 1, y0 - 1))
+            possible_move = Move(src_cell, dest_cell, change)
+            possible_moves.append(possible_move)
+
+        if (x0 < board_width - 1 and y0 > 0): # Move bottom right
+            dest_cell = self.__board.get_cell((x0 + 1, y0 - 1))
+            possible_move = Move(src_cell, dest_cell, change)
+            possible_moves.append(possible_move)
+
+        if (x0 > 0 and y0 < board_height - 1): # Move up left
+            dest_cell = self.__board.get_cell((x0 - 1, y0 + 1))
+            possible_move = Move(src_cell, dest_cell, change)
+            possible_moves.append(possible_move)
+    
         if (x0 < board_width - 1 and y0 < board_height - 1): # Move up right
             dest_cell = self.__board.get_cell((x0 + 1, y0 + 1))
             possible_move = Move(src_cell, dest_cell, change)
             possible_moves.append(possible_move)
-        if (x0 < board_width - 1 and ): # Move bottom right
-            dest_cell = self.__board.get_cell((x0 + 1, y0 - 1))
-            possible_move = Move(src_cell, dest_cell, change)
-            possible_moves.append(possible_move)
+
         return possible_moves
 
 class Move:
