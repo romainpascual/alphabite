7180afb349383888e364218928c171dd4b3bc69c
Bouziane Othmane <othmane.bouziane@student.ecp.fr>
2019-02-25 15:56:30 +0100

    private

--- ia/parcours.py
+++ ia/parcours.py
@@ -10,10 +10,10 @@ import time
 
 class IA:
     def __init__(self, src_board, socket):
-        self.src_board = src_board
-        self.best_move = None
-        self.generator = BoardGenerator(src_board)
-        self.socket = socket
+        self.__src_board = src_board
+        self.__best_move = None
+        self.__generator = BoardGenerator(src_board)
+        self.__socket = socket
 
     def alphabeta(self, src_board, depth=0, isMaximizingPlayer=True, alpha=-float('inf'), beta=float('inf'), max_depth=5):
         """
@@ -38,13 +38,13 @@ class IA:
         if isMaximizingPlayer:
             best_val = -float('inf')
             best_move = None
-            for (board, move) in self.generator.get_all_possible_boards():
+            for (board, move) in self.__generator.get_all_possible_boards():
                 value = self.alphabeta(board, depth + 1, False, alpha, beta, max_depth)
                 if best_val < value:
                     best_val = value
                     best_move = move
                     if depth == 0:
-                        self.best_move = best_move
+                        self.__best_move = best_move
                 alpha = max(alpha, best_val)
                 if beta <= alpha:
                     break
@@ -53,24 +53,24 @@ class IA:
         else:
             best_val = float('inf')
             best_move = None
-            for (board, move) in self.generator.get_all_possible_boards():
+            for (board, move) in self.__generator.get_all_possible_boards():
                 value = self.alphabeta(board, depth + 1, True, alpha, beta, max_depth)
                 if best_val < value:
                     best_val = value
                     best_move = move
                     if depth == 0:
-                        self.best_move = best_move
+                        self.__best_move = best_move
                 beta = min(beta, best_val)
                 if beta <= alpha:
                     break
             return best_val, best_move
 
     def run(self, max_depth):
-        self.alphabeta(self.src_board, 0, True, -float('inf'), float('inf'), max_depth)
+        self.alphabeta(self.__src_board, 0, True, -float('inf'), float('inf'), max_depth)
         # tic = time.time()
         # while time.time()-tic <= 2:
         #     continue
-        self.socket._send_mov(self.best_move.parse_for_socket())
+        self.__socket.send_mov(self.__best_move.parse_for_socket())
 
 #Pour lancer le alphabeta sur un arbre il faut lancer la fonction
 
