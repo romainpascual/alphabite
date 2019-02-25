acbd241d58991e4777324289658d51259b49c7be
adam-hotait <adam.hotait@student.ecp.fr>
2019-02-25 15:21:15 +0100

    feat: connect program to board

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 from servercomms.servercomms import SocketConnector
+from board.board import Board
 
 if len(sys.argv) < 3:
     raise IOError("At least 2 arguments are required (IP and port).")
@@ -8,10 +9,10 @@ ip = sys.argv[-2]
 port = int(sys.argv[-1])
 
 socket = SocketConnector(ip, port)
+board = Board()
 
-socket.board_set = lambda n, m: print("Grid size : {}x{}".format(n, m))
-socket.board_map = lambda upd: print("Map:", upd)
-socket.board_upd = lambda upd: print("UPD:", upd)
+socket.set_methods(board.build, board.set_species, board.update, board.update)
 
+socket.launch_game()
 socket.start()
 socket.join()
