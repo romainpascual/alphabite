5297646df05d78b6736647a9dc36ef4d3f522e7e
Arthur Caillaud <arthurcaillaud@gmail.com>
2019-02-25 14:10:37 +0100

    There was an undefined variable in misc.py

--- board_generator/misc.py
+++ board_generator/misc.py
@@ -4,5 +4,5 @@ from functools import reduce
 def comb(n, k): # Computes k parmi n
     k = min(k, n-k)
     numer = reduce(op.mul, range(n, n-k, -1), 1)
-    denom = reduce(op.mul, range(1, r+1), 1)
+    denom = reduce(op.mul, range(1, k+1), 1)
     return numer / denom
