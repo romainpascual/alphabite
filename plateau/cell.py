0986f1ceb9d5c9fc9f35dca4ab5e0d1cfd486958
Romain Pascual <romain.pascual@student.ecp.fr>
2019-02-25 14:11:06 +0100

    cell functions

--- plateau/cell.py
+++ plateau/cell.py
@@ -14,5 +14,32 @@ class Board( object ):
     # -- INITIALIZATION
     # ----------------------------------------------------------------------------
 
-    def __init__(self):
-        pass
+    def __init__(self, x, y, species, group_size):
+        self.__x = x
+        self.__y = y
+        self.__species = species
+        self.__group_size = group_size
+
+    # END def __init__
+
+    # ----------------------------------------------------------------------------
+    # -- GETTERS AND SETTERS
+    # ----------------------------------------------------------------------------
+
+    def get_x(self):
+        return self.__x
+    
+    def get_y(self):
+        return self.__y
+    
+    def get_species(self):
+        return self.__species
+
+    def get_group_size(self):
+        return self.__group_size 
+    
+    def set_x(self, x):
+        self.__x = x
+    
+    def set_y(self, y):
+        self.__y = y
