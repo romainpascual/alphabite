0342e7701022db0b4d9588e07c3738d22e82e0da
Arthur Caillaud <arthurcaillaud@gmail.com>
2019-03-04 16:45:01 +0100

    First version of new MoveGenerator with prev_move

--- ia/search.py
+++ ia/search.py
@@ -27,7 +27,7 @@ class IA(Thread):
         self.__my_species = species
         self.__enemy_species = 'v' if self.__my_species == 'w' else 'w'
 
-    def alphabeta(self, src_board, depth=0, isMaximizingPlayer=True, alpha=-float('inf'), beta=float('inf'), max_depth=5):
+    def alphabeta(self, src_board, prev_move=None, depth=0, isMaximizingPlayer=True, alpha=-float('inf'), beta=float('inf'), max_depth=5):
         """
         Alphabeta AI to choose the best move to play
         :param src_board: Actual Board on which we apply Alphabeta
@@ -43,12 +43,12 @@ class IA(Thread):
             return src_board.heuristic(self.__my_species)
 
         if isMaximizingPlayer:
-            generator = BoardGenerator(src_board, self.__my_species)
+            generator = BoardGenerator(src_board, self.__my_species, prev_move)
             best_val = -float('inf')
             for board_with_probability, move in generator.get_all_possible_boards():
                 board = board_with_probability[0]
                 p = board_with_probability[1]
-                value = p * self.alphabeta(board, depth + 1, False, alpha, beta, max_depth)
+                value = p * self.alphabeta(board, depth + 1, move, False, alpha, beta, max_depth)
                 if best_val < value:
                     best_val = value
                     if depth == 0:
@@ -59,12 +59,12 @@ class IA(Thread):
             return best_val
 
         else:
-            generator = BoardGenerator(src_board, self.__enemy_species)
+            generator = BoardGenerator(src_board, self.__enemy_species, prev_move)
             best_val = float('inf')
             for board_with_probability, move in generator.get_all_possible_boards():
                 board = board_with_probability[0]
                 p = board_with_probability[1]
-                value = p * self.alphabeta(board, depth + 1, True, alpha, beta, max_depth)
+                value = p * self.alphabeta(board, depth + 1, move, True, alpha, beta, max_depth)
                 if value < best_val:
                     best_val = value
                     if depth == 0:
@@ -88,6 +88,7 @@ class IA(Thread):
                 tic = time.time()
                 self.alphabeta(self.__src_board,
                                depth=0,
+                               move=None,
                                isMaximizingPlayer=True,
                                alpha=-float('inf'),
                                beta=float('inf'),
