a0a7fa651f9f9ce79a77b662b801d55026af9f36
Arthur Caillaud <arthurcaillaud@gmail.com>
2019-03-04 16:46:45 +0100

    prev_move may be None

--- board_generator/move_generator.py
+++ board_generator/move_generator.py
@@ -34,72 +34,115 @@ class MoveGenerator:
         board_height = self.__board.height
         x0 = src_cell.x
         y0 = src_cell.y
-        prev_cell = self.__prev_move.src_cell
-        delta_x = x0 - prev_cell.x
-        delta_y = y0 - prev_cell.y
 
-        if x0 > 0 and y0 > 0
-            and not (delta_x == 1 and delta_y == 1)
-            and not (delta_x == 0 and delta_y == 1)
-            and not (delta_x == 1 and delta_y == 0):  # Move up left
-            dest_cell = self.__board.get_cell((x0 - 1, y0 - 1))
-            possible_move = Move(src_cell, dest_cell, change)
-            possible_moves.append(possible_move)
+        if self.__prev_move:
+            prev_cell = self.__prev_move.src_cell
+            delta_x = x0 - prev_cell.x
+            delta_y = y0 - prev_cell.y
 
-        if x0 < board_width - 1 and y0 > 0
-            and not (delta_x == 0 and delta_y == 1)
-            and not (delta_x == -1 and delta_y == 0)
-            and not (delta_x == -1 and delta_y == 1):  # Move up right
-            dest_cell = self.__board.get_cell((x0 + 1, y0 - 1))
-            possible_move = Move(src_cell, dest_cell, change)
-            possible_moves.append(possible_move)
+            if x0 > 0 and y0 > 0
+                and not (delta_x == 1 and delta_y == 1)
+                and not (delta_x == 0 and delta_y == 1)
+                and not (delta_x == 1 and delta_y == 0):  # Move up left
+                dest_cell = self.__board.get_cell((x0 - 1, y0 - 1))
+                possible_move = Move(src_cell, dest_cell, change)
+                possible_moves.append(possible_move)
 
-        if x0 > 0 and y0 < board_height - 1
-            and not (delta_x == 1 and delta_y == 0)
-            and not (delta_x == 1 and delta_y == -1)
-            and not (delta_x == 0 and delta_y == -1):  # Move down left
-            dest_cell = self.__board.get_cell((x0 - 1, y0 + 1))
-            possible_move = Move(src_cell, dest_cell, change)
-            possible_moves.append(possible_move)
+            if x0 < board_width - 1 and y0 > 0
+                and not (delta_x == 0 and delta_y == 1)
+                and not (delta_x == -1 and delta_y == 0)
+                and not (delta_x == -1 and delta_y == 1):  # Move up right
+                dest_cell = self.__board.get_cell((x0 + 1, y0 - 1))
+                possible_move = Move(src_cell, dest_cell, change)
+                possible_moves.append(possible_move)
 
-        if x0 < board_width - 1 and y0 < board_height - 1
-            and not (delta_x == -1 and delta_y == 0)
-            and not (delta_x == -1 and delta_y == -1)
-            and not (delta_x == 0 and delta_y == -1):  # Move down right
-            dest_cell = self.__board.get_cell((x0 + 1, y0 + 1))
-            possible_move = Move(src_cell, dest_cell, change)
-            possible_moves.append(possible_move)
+            if x0 > 0 and y0 < board_height - 1
+                and not (delta_x == 1 and delta_y == 0)
+                and not (delta_x == 1 and delta_y == -1)
+                and not (delta_x == 0 and delta_y == -1):  # Move down left
+                dest_cell = self.__board.get_cell((x0 - 1, y0 + 1))
+                possible_move = Move(src_cell, dest_cell, change)
+                possible_moves.append(possible_move)
 
-        if x0 > 0
-            and not (delta_x == 1 and delta_y == 1)
-            and not (delta_x == 1 and delta_y == 0)
-            and not (delta_x == 1 and delta_y == -1): # Move left
-            dest_cell = self.__board.get_cell((x0 - 1, y0))
-            possible_move = Move(src_cell, dest_cell, change)
-            possible_moves.append(possible_move)
+            if x0 < board_width - 1 and y0 < board_height - 1
+                and not (delta_x == -1 and delta_y == 0)
+                and not (delta_x == -1 and delta_y == -1)
+                and not (delta_x == 0 and delta_y == -1):  # Move down right
+                dest_cell = self.__board.get_cell((x0 + 1, y0 + 1))
+                possible_move = Move(src_cell, dest_cell, change)
+                possible_moves.append(possible_move)
 
-        if x0 < board_width - 1 and delta_x > 0
-            and not (delta_x == -1 and delta_y == 1)
-            and not (delta_x == -1 and delta_y == 0)
-            and not (delta_x == -1 and delta_y == -1): # Move right
-            dest_cell = self.__board.get_cell((x0 + 1, y0))
-            possible_move = Move(src_cell, dest_cell, change)
-            possible_moves.append(possible_move)
+            if x0 > 0
+                and not (delta_x == 1 and delta_y == 1)
+                and not (delta_x == 1 and delta_y == 0)
+                and not (delta_x == 1 and delta_y == -1): # Move left
+                dest_cell = self.__board.get_cell((x0 - 1, y0))
+                possible_move = Move(src_cell, dest_cell, change)
+                possible_moves.append(possible_move)
 
-        if y0 > 0
-            and not (delta_x == 0 and delta_y == 1)
-            and not (delta_x == 1 and delta_y == 1)
-            and not (delta_x == -1 and delta_y == 1):  # Move up
-            dest_cell = self.__board.get_cell((x0, y0 - 1))
-            possible_move = Move(src_cell, dest_cell, change)
-            possible_moves.append(possible_move)
+            if x0 < board_width - 1 and delta_x > 0
+                and not (delta_x == -1 and delta_y == 1)
+                and not (delta_x == -1 and delta_y == 0)
+                and not (delta_x == -1 and delta_y == -1): # Move right
+                dest_cell = self.__board.get_cell((x0 + 1, y0))
+                possible_move = Move(src_cell, dest_cell, change)
+                possible_moves.append(possible_move)
 
-        if y0 < board_height - 1
-            and not (delta_x == 0 and delta_y == -1)
-            and not (delta_x == 1 and delta_y == -1)
-            and not (delta_x == -1 and delta_y == -1):  # Move down
-            dest_cell = self.__board.get_cell((x0, y0 + 1))
-            possible_move = Move(src_cell, dest_cell, change)
-            possible_moves.append(possible_move)
+            if y0 > 0
+                and not (delta_x == 0 and delta_y == 1)
+                and not (delta_x == 1 and delta_y == 1)
+                and not (delta_x == -1 and delta_y == 1):  # Move up
+                dest_cell = self.__board.get_cell((x0, y0 - 1))
+                possible_move = Move(src_cell, dest_cell, change)
+                possible_moves.append(possible_move)
+
+            if y0 < board_height - 1
+                and not (delta_x == 0 and delta_y == -1)
+                and not (delta_x == 1 and delta_y == -1)
+                and not (delta_x == -1 and delta_y == -1):  # Move down
+                dest_cell = self.__board.get_cell((x0, y0 + 1))
+                possible_move = Move(src_cell, dest_cell, change)
+                possible_moves.append(possible_move)
+
+        else:
+            if x0 > 0 and y0 > 0:  # Move up left
+                dest_cell = self.__board.get_cell((x0 - 1, y0 - 1))
+                possible_move = Move(src_cell, dest_cell, change)
+                possible_moves.append(possible_move)
+
+            if x0 < board_width - 1 and y0 > 0:  # Move up right
+                dest_cell = self.__board.get_cell((x0 + 1, y0 - 1))
+                possible_move = Move(src_cell, dest_cell, change)
+                possible_moves.append(possible_move)
+
+            if x0 > 0 and y0 < board_height - 1:  # Move down left
+                dest_cell = self.__board.get_cell((x0 - 1, y0 + 1))
+                possible_move = Move(src_cell, dest_cell, change)
+                possible_moves.append(possible_move)
+
+            if x0 < board_width - 1 and y0 < board_height - 1:  # Move down right
+                dest_cell = self.__board.get_cell((x0 + 1, y0 + 1))
+                possible_move = Move(src_cell, dest_cell, change)
+                possible_moves.append(possible_move)
+
+            if x0 > 0: # Move left
+                dest_cell = self.__board.get_cell((x0 - 1, y0))
+                possible_move = Move(src_cell, dest_cell, change)
+                possible_moves.append(possible_move)
+
+            if x0 < board_width - 1 and delta_x > 0: # Move right
+                dest_cell = self.__board.get_cell((x0 + 1, y0))
+                possible_move = Move(src_cell, dest_cell, change)
+                possible_moves.append(possible_move)
+
+            if y0 > 0:  # Move up
+                dest_cell = self.__board.get_cell((x0, y0 - 1))
+                possible_move = Move(src_cell, dest_cell, change)
+                possible_moves.append(possible_move)
+
+            if y0 < board_height - 1:  # Move down
+                dest_cell = self.__board.get_cell((x0, y0 + 1))
+                possible_move = Move(src_cell, dest_cell, change)
+                possible_moves.append(possible_move)
 
         return possible_moves
