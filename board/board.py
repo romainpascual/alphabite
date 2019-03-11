a3a7e3d4b1720db56bc65b92a69b39d03fae90be
Romain Pascual <romain.pascual@student.ecp.fr>
2019-03-11 15:06:53 +0100

    delete size storage

--- board/board.py
+++ board/board.py
@@ -448,14 +448,14 @@ class Board:
                 specie_value = (self.__w - self.__v)/(self.__w + self.__v)
 
                 # if the ratio is > 1, we want to minimze the distance
-                dist_value = self.__vw_min[0] * self.f(float(self.__vw_min[2].group_size)/float(self.__vw_min[1].group_size)) / ((self.X + self.Y)/2)
+                dist_value = self.__vw_min[0] * self.f(float(self.__vw_min[2].group_size)/float(self.__vw_min[1].group_size)) / ((self.__X + self.__Y)/2)
 
                 # we want to maximize the distance for between the other specie and a human cell
                 human_value = self.__vh_min[0]
                 # we want to minimize the distance between our species and a human cell
                 human_value -= self.__wh_min[0]
                 # turn in non dependant on the board size
-                human_value /= ((self.X + self.Y)/2)
+                human_value /= ((self.__X + self.__Y)/2)
         
         else:
             if self.__v == 0:
@@ -464,8 +464,8 @@ class Board:
                 return win_value, 1
             else:
                 specie_value = (self.__v - self.__w)/(self.__w + self.__v)
-                dist_value = (self.__vw_min[0] * self.f(float(self.__vw_min[1].group_size)/float(self.__vw_min[2].group_size))) / ((self.X + self.Y)/2)
-                human_value = (self.__wh_min[0] - self.__vh_min[0])/ ((self.X + self.Y)/2)
+                dist_value = (self.__vw_min[0] * self.f(float(self.__vw_min[1].group_size)/float(self.__vw_min[2].group_size))) / ((self.__X + self.__Y)/2)
+                human_value = (self.__wh_min[0] - self.__vh_min[0])/ ((self.__X + self.__Y)/2)
             
         #print("specie_value: {} -- dist_value: {} -- human_value: {}".format(specie_value, dist_value, human_value))
         output_value = specie_value*alpha_specie + dist_value*alpha_dist + human_value*alpha_human
