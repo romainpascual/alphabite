75aeaa36c1b8d5bcdc782efcd57c2149baf6dbf2
adam-hotait <adam.hotait@student.ecp.fr>
2019-03-11 15:42:06 +0100

    chore: PEP8

--- board_generator/misc.py
+++ board_generator/misc.py
@@ -4,6 +4,7 @@ from functools import reduce
 comb_hash = {}
 p_binom_hash = {}
 
+
 def comb(n, k):  # Computes n choose k
     hash_string = "{0}:{1}".format(n, k)
     if not hasattr(comb_hash, hash_string):
@@ -12,6 +13,7 @@ def comb(n, k):  # Computes n choose k
         denom = reduce(op.mul, range(1, k+1), 1)
         comb_hash[hash_string] = numer / denom
     return comb_hash[hash_string]
+
 
 def p_binom(n, k, p):
     hash_string = "{0}:{1}:{2}".format(n, k, p)
